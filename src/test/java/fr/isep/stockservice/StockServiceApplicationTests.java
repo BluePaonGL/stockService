10212d69f50ae98c562883d2ed3838e22124ba52
Cécile <74317560+Cecile-L@users.noreply.github.com>
2022-05-12 22:03:14 +0200

    Initial commit

---
+++ src/test/java/fr/isep/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isep.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
