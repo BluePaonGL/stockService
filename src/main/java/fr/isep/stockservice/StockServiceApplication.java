caec4fbe956b4326654da964a3adbaf0de0d13e7
MightySlackness <fb2luKm@gmail.com>
2022-05-12 23:29:43 +0200

    test
    glbskf

--- src/main/java/fr/isep/stockservice/StockServiceApplication.java
+++ src/main/java/fr/isep/stockservice/StockServiceApplication.java
@@ -8,6 +8,7 @@ public class StockServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StockServiceApplication.class, args);
+        System.out.println("ceci est un test lmao");
     }
 
 }
